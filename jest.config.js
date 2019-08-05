fedc4b9b1ad90c0ae696b9abe4808e54ab3163a2
Tõnis Kasekamp <toniskasekamp@gmail.com>
2019-08-05 19:11:32 +0300

    Code coverage task

--- jest.config.js
+++ jest.config.js
@@ -14,6 +14,7 @@ module.exports = {
     testMatch: [
         "**/test/**/*.test.(ts|js)"
     ],
+    coverageReporters: ["text", "cobertura", "html"], 
     reporters: [ "default", "jest-junit" ],
     testEnvironment: "node"
 };
